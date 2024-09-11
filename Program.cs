e91627462f7d80bcc6052b933c32b7d8d152ba4b
Ioannis Terpo <teioannis@uth.gr>
2024-09-11 18:30:25 +0300

    retouching codebase before starting over v1

--- Program.cs
+++ Program.cs
@@ -18,17 +18,6 @@ namespace Kino
             int[] kinoTable = new int[Constants.numbersInArray];
             int[] computerNumbersTable = new int[Constants.computerChoose];
 
-            /* for (int fillKinoTable = 0; fillKinoTable < Constants.numbersInArray; fillKinoTable++)
-             {
-                 kinoTable[fillKinoTable] = 0;
-                 if ( fillKinoTable == 10 || fillKinoTable == 20 || fillKinoTable == 30 || fillKinoTable == 40 || fillKinoTable == 50 || fillKinoTable==60 || fillKinoTable == 70 ||fillKinoTable == 80)
-                 {
-                     Console.Write("\n");
-                 }
-                 //Console.WriteLine(fillKinoTable); | Checks if we did it 
-                 Console.Write($"| {kinoTable[fillKinoTable]} |");
-             }*/
-
             // Ask User to Play
             Console.WriteLine("Let's start playing \n");
             Console.WriteLine("Pick a Number from 1 to 12 - (this will the amount of numbers you will choose)");
@@ -36,7 +25,7 @@ namespace Kino
             int howManyNumbers = Convert.ToInt32(Console.ReadLine());
             int[] numberUser = new int[howManyNumbers]; //creates an ARRAY with x elements -- number of elements chosen by the user too.
 
-            for(int i = 0; i < howManyNumbers; i++)
+            for (int i = 0; i < howManyNumbers; i++)
             {
                 //Lets fill the array with user's numbers
                 Console.WriteLine("Pick a number from 1 to 80 \n"); // ? Problem - the user may not listen print and choose another number ? 
@@ -44,26 +33,28 @@ namespace Kino
                 numberUser[i] = numberPicked;
                 Console.WriteLine($"The number {numberPicked} - was added to your List \n");
             }
+
             Console.WriteLine("|Yours Numbers are| \n");
+
             for (int y = 0; y < howManyNumbers; y++)
             {
                 Console.Write($" |{numberUser[y]}|");
             }
 
 
-                Console.Write("\n \n"); //SKIP LINES
+            Console.Write("\n \n"); //SKIP LINES
             var generator = new RandomGenerator();
-            for (int choosingLoop=0; choosingLoop< Constants.computerChoose; choosingLoop++)
+            for (int choosingLoop = 0; choosingLoop < Constants.computerChoose; choosingLoop++)
             {
                 //var generator = new RandomGenerator();
                 var randomNumber = generator.RandomNumber(1, 80); //problem -- the random numbers array may contain same numbers ! 
-                for ( int i = 0; i < Constants.computerChoose; i++ )
+                for (int i = 0; i < Constants.computerChoose; i++)
                 {
-                    if (computerNumbersTable[i] == randomNumber )
+                    if (computerNumbersTable[i] == randomNumber)
                     {
-                        i = 0 ; // Re - DO the for loop to Re-CHECK. 
+                        i = 0; // Re - DO the for loop to Re-CHECK. 
                         randomNumber = generator.RandomNumber(1, 80);  // Assign New Random Number
-                     }
+                    }
 
                 }
                 Console.WriteLine($"The machine picked the number: {randomNumber}");
@@ -71,7 +62,7 @@ namespace Kino
                 kinoTable[randomNumber - 1]++;
             }
 
-        
+
         }
     }
     public class RandomGenerator
@@ -88,3 +79,8 @@ namespace Kino
         }
     }
 }
+
+
+
+// TODO: 1. User can not pick dublicates. 
+//       2. Visualize all the numbers 1-80 and maybe the numbers picked from machine with "()".
