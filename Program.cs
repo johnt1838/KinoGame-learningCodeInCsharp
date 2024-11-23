088b41f19af75aaacf577c86b48ce0d75ffec2cf
Ioannis Terpo <teioannis@uth.gr>
2024-11-23 22:11:22 +0200

    update simple algs

--- Program.cs
+++ Program.cs
@@ -19,23 +19,35 @@ namespace Kino
             int[] computerNumbersTable = new int[Constants.computerChoose];
 
             // Ask User to Play
-            Console.WriteLine("Let's start playing \n");
+            Console.WriteLine("Let's Start !\n");
             Console.WriteLine("Pick a Number from 1 to 12 - (this will the amount of numbers you will choose)");
 
             int howManyNumbers = Convert.ToInt32(Console.ReadLine());
             int[] numberUser = new int[howManyNumbers]; //creates an ARRAY with x elements -- number of elements chosen by the user too.
 
-            for (int i = 0; i < howManyNumbers; i++)
-            {
-                //Lets fill the array with user's numbers
+            int counter = 0;
+            while (counter < howManyNumbers){
                 Console.WriteLine("Pick a number from 1 to 80 \n"); // ? Problem - the user may not listen print and choose another number ? 
                 int numberPicked = Convert.ToInt32(Console.ReadLine());
-                numberUser[i] = numberPicked;
+                if (numberPicked < 1 || numberPicked > 80)
+                {
+                    Console.WriteLine("Invalid number. Please choose a number between 1 and 80.");
+                    continue;
+                }
+                if (Array.IndexOf(numberUser, numberPicked) != -1)
+                {
+                    Console.WriteLine("This number is already in your list. Try again.");
+                    continue;
+                }
+                numberUser[counter] = numberPicked;
+                counter++;
                 Console.WriteLine($"The number {numberPicked} - was added to your List \n");
             }
 
-            Console.WriteLine("|Yours Numbers are| \n");
 
+
+
+            Console.WriteLine("|Yours Numbers are| \n");
             for (int y = 0; y < howManyNumbers; y++)
             {
                 Console.Write($" |{numberUser[y]}|");
@@ -44,10 +56,11 @@ namespace Kino
 
             Console.Write("\n \n"); //SKIP LINES
             var generator = new RandomGenerator();
+            Console.WriteLine($"The machine picked the numbers: \n");
             for (int choosingLoop = 0; choosingLoop < Constants.computerChoose; choosingLoop++)
             {
                 //var generator = new RandomGenerator();
-                var randomNumber = generator.RandomNumber(1, 80); //problem -- the random numbers array may contain same numbers ! 
+                var randomNumber = generator.RandomNumber(1, 80); //todo: problem -- the random numbers array may contain same numbers ! 
                 for (int i = 0; i < Constants.computerChoose; i++)
                 {
                     if (computerNumbersTable[i] == randomNumber)
@@ -55,9 +68,8 @@ namespace Kino
                         i = 0; // Re - DO the for loop to Re-CHECK. 
                         randomNumber = generator.RandomNumber(1, 80);  // Assign New Random Number
                     }
-
                 }
-                Console.WriteLine($"The machine picked the number: {randomNumber}");
+                Console.Write($"({randomNumber}) ");
                 computerNumbersTable[choosingLoop] = randomNumber;
                 kinoTable[randomNumber - 1]++;
             }
@@ -65,6 +77,9 @@ namespace Kino
 
         }
     }
+
+
+
     public class RandomGenerator
     {
         // Instantiate random number generator.  
@@ -72,15 +87,10 @@ namespace Kino
         // and keep using Next on the same instance.  
         private readonly Random _random = new Random();
 
-        // Generates a random number within a range.      
+        // Generates a random number within specified range.      
         public int RandomNumber(int min, int max)
         {
             return _random.Next(min, max);
         }
     }
 }
-
-
-
-// TODO: 1. User can not pick dublicates. 
-//       2. Visualize all the numbers 1-80 and maybe the numbers picked from machine with "()".
